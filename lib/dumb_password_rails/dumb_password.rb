5b09aab40d2e941446b99a17099051a98d8a72d7
Okeke Arinze <trent9002@gmail.com>
2017-04-25 18:03:56 +0100

    add validate_each method

--- lib/dumb_password_rails/dumb_password.rb
+++ lib/dumb_password_rails/dumb_password.rb
@@ -1,15 +1,25 @@
 module DumbPasswordRails
 
 	class DumbPassword < ActiveModel::EachValidator
-		def read_passwords
+		def dumb(value)
 		 	passwordFile = File.open('/../../resources/passwordlist.txt', 'r')
 		 	passwords = mf.readlines
 		 	passwords.map! {|d| d.strip}
 		 	passwords = con.sort
-		 	if passwords.include?(password)
+		 	return true if passwords.include?(password)
+		 	false
+		 		r
 		 		errors.add(:password, "Your password is too common. Use a more secure password")
-		 	end
 		end
+
+		def validate_each(record, attribute, value)
+    		#options = @@default_options.merge(self.options)
+
+    		if self.class.dumb?(value)
+    			message = "Your password is too common. Use a more secure password"
+      			record.errors.add(attribute, message || :invalid)
+    		end
+  		end
 	end
 
 end
